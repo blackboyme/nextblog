743568f85bdf57184efea4da3bfa7880546f6497
blackboyme <254832710@qq.com>
2024-11-12 14:35:50 +0800

    fabars

--- src/components/PhoneBars.js
+++ src/components/PhoneBars.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faDice } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function PhoneBars() {
     return (
         <>
-            <FontAwesomeIcon icon={faDice} className='text-black w-6 h-6 cursor-pointer sm:hidden mr-2' />
-            <FontAwesomeIcon icon={faBars} className='text-black w-6 h-6 cursor-pointer sm:hidden' />
-
+          <FontAwesomeIcon icon={faBars} className='text-black w-6 h-6 cursor-pointer sm:hidden' />
         </>
     )
 }
