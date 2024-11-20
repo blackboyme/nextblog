5a63a34a7f47158480e6f8fedd03d61125ea87be
blackboyme <254832710@qq.com>
2024-11-21 01:01:49 +0800

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,27 @@ export default function Home() {
       <Hero />
       <main id="wrapper" className="w-full bg-blue-100">
         <Banner />
+        <div className="container mx-auto w-full flex">
+          <section className="flex flex-col flex-[0.75] ">
+              <div className="flex justify-start items-center gap-4 bg-white border-gray-500  h-20 rounded-lg p-3">
+                  <div className="p-2 bg-blue-500 rounded-lg text-white">
+                    首页
+                  </div>
+                  <div>
+                    感情
+                  </div>
+                  <div>
+                    恋爱
+                  </div>
+              </div>
+              <div className="bg-green-100 h-full">
+
+              </div>
+          </section>
+          <section>
+            
+          </section>
+        </div>
       </main>
     </div>
   );
