f093230466c85550640c29554420f31467c6af35
blackboyme <254832710@qq.com>
2024-11-24 23:17:21 +0800

    personal

--- src/components/PesonalInfo.js
+++ src/components/PesonalInfo.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBilibili, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function PesonalInfo() {
     return (
@@ -6,10 +8,10 @@ export default function PesonalInfo() {
             <div className='mt-3 group/tip py-[2px] px-2 bg-[#7587F3] hover:bg-white rounded-full cursor-pointer'>
                 <span className='text-[12px] group-hover/tip:text-[14px] group-hover/tip:text-gray-900 text-white'>不要太熬夜了，早睡更健康</span>
             </div>
-            <div className='hidden group-hover:block mt-10 w-full px-6 min-h-40'>
+            <div className='hidden group-hover:block mt-6 w-full px-6 min-h-40'>
                 <span className='text-white'>你好啊，我是Gmoonlight1。我的MBTI是ISFJ。这是一种能干、肯干的个性类型，并且多才多艺。很高兴认识你！</span>
             </div>
-            <div className='relative flex group-hover:hidden mt-10 w-full px-6 justify-center items-center min-h-40'>
+            <div className='relative flex group-hover:hidden mt-6 w-full px-6 justify-center items-center min-h-40'>
                 <div className='flex items-center justify-center bg-white w-[136px] h-[136px] rounded-full'>
                     <div className='flex items-center justify-center bg-[#5268F0] w-32 h-32 rounded-full'>
                         <img src='/okgirl.png' alt='184064199' className='w-32 h-32 rounded-full'></img>
@@ -19,8 +21,23 @@ export default function PesonalInfo() {
                     <img src='/bilismile.gif' alt='184064199' className='w-6 h-6 rounded-full'></img>
                 </div>
             </div>
-            <div className='flex items-center justify-center bg-white w-full h-10 rounded-full'>
-                <span className='text-gray-900'>点击头像查看更多信息</span>
+            <div className=' flex items-center justify-between w-full p-6'>
+                <div className=''>
+                    <div className='text-white text-xl'>
+                        Gmoonlight1
+                    </div>
+                    <div className='text-gray-200 text-[12px]'>
+                        幸好你也喜欢秋天
+                    </div>
+                </div>
+                <div className='flex'>
+                    <div className='bg-[#7587F3] w-11 h-11 rounded-full flex items-center justify-center mr-2'>
+                        <FontAwesomeIcon icon={faGithub} className='text-white w-8 h-8 cursor-pointer' />
+                    </div>
+                    <div className='bg-[#7587F3] w-11 h-11 rounded-full flex items-center justify-center '>
+                        <FontAwesomeIcon icon={faBilibili} className='text-white w-8 h-8 cursor-pointer' />
+                    </div>
+                </div>
             </div>
         </div>
     )
