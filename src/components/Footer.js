2cc8e4db7e03b95fa0a00aba5ac6d8eb6822c491
blackboyme <254832710@qq.com>
2024-11-25 23:36:53 +0800

    footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='w-full bg-gray-300 py-6'>
+        <div className='w-full bg-[#F7F7F9] py-6'>
             <div className='flex container mx-auto justify-between'>
                 <div>
                     <span>@2024 - 2024 By </span>
